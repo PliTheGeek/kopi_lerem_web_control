6677a98950f613739d15ee30dc22e8c5037450c1
PliTheGeek <dhafin.kamil@gmail.com>
2026-02-14 22:16:20 +0700

    Modify some json endpoint parse

--- public/app.js
+++ public/app.js
@@ -1,23 +1,26 @@
-// Inisialisasi koneksi socket
-const socket = io('http://20.6.33.91');
-
-// PASTIKAN: Nama event harus sama dengan io.emit di server.js ('mqtt-data')
 socket.on('mqtt-data', (data) => {
     console.log("Menerima update dari Wonosobo:", data);
 
-    // 1. Update Kelembapan
-    if (data.topic === 'greenhouse/humi') {
-        const humidityElement = document.getElementById('humidity-value');
-        if (humidityElement) {
-            humidityElement.innerText = data.message; // Pakai .message sesuai server.js
-        }
-    } 
-    
-    // 2. Update Temperatur
-    if (data.topic === 'greenhouse/temp') {
-        const tempElement = document.getElementById('temperature-value');
-        if (tempElement) {
-            tempElement.innerText = data.message; // Pakai .message sesuai server.js
+    // Filter topik sesuai yang dikirim ESP32
+    if (data.topic === 'greenhouse/sensors') {
+        try {
+            // Membongkar bungkusan JSON dari string menjadi object
+            const payload = JSON.parse(data.message);
+
+            // Update Humidity ke <span id="humidity-value">
+            const humidityElement = document.getElementById('humidity-value');
+            if (humidityElement && payload.hum !== undefined) {
+                humidityElement.innerText = payload.hum;
+            }
+
+            // Update Temperature ke <span id="temperature-value">
+            const tempElement = document.getElementById('temperature-value');
+            if (tempElement && payload.temp !== undefined) {
+                tempElement.innerText = payload.temp;
+            }
+
+        } catch (e) {
+            console.error("Format data rusak atau bukan JSON:", e);
         }
     }
 });
